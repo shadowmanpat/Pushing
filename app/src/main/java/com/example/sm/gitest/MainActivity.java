aad276658e91e574f627f796dacd66de2b9a79cc
Nikolaos Agas <nagas@vanderbox.gr>
2017-10-12 13:20:01 +0300

    altihhis

--- app/src/main/java/com/example/sm/gitest/MainActivity.java
+++ app/src/main/java/com/example/sm/gitest/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void TestUpload(){
+        //3+3
+        boolean alitheia=true;
         int i=0;
 
         i++;
