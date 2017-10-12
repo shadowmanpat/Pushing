f61c4aa6d694b2c48e356764588162dea5bef591
Nikolaos Agas <nagas@vanderbox.gr>
2017-10-12 12:20:00 +0300

    Modified MainActivity Added new File TestAddFile

--- app/src/main/java/com/example/sm/gitest/MainActivity.java
+++ app/src/main/java/com/example/sm/gitest/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     }
     public void TestUpload(){
         int i=0;
+
+        i++;
     }
 }
---
+++ app/src/main/java/com/example/sm/gitest/TestAddGFile.java
@@ -0,0 +1,8 @@
+package com.example.sm.gitest;
+
+/**
+ * Created by sm on 10/12/17.
+ */
+
+public class TestAddGFile {
+}
